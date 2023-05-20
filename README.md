3852cb86145131f835d3cd6743c0c40b0d975588
PShivers <paulsshivers@gmail.com>
2023-05-19 20:34:16 -0400

    finished part 2 of tutorial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # angular-first-app
+
+Following along with tutorial found [here](https://angular.io/tutorial/first-app).
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root', // this seems to be selecting the "root" HTML element in the index.html
   standalone: true,
-  imports: [],
-  template: `<h1>Hello world!</h1>`,
+  imports: [HomeComponent],
+  template: `
+    <main>
+      <header>
+        <img class="brand-name" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+
+      </section>
+    </main>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
